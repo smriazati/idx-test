b5bba4f8ab11ddcc5983786b295bd26413d9bbb4
smriazati <sarahriazati@gmail.com>
2022-09-12 15:03:35 -0400

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="layout-container layout-default" :class="thisRouteClass">
+    <nuxt :key="$route.fullPath" />
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    thisRouteClass() {
+      const path = this.$route.fullPath;
+      let thisRouteClass;
+      if (path === "/") {
+        thisRouteClass = "home";
+      } else {
+        thisRouteClass = path.replaceAll("/", "-");
+      }
+      return thisRouteClass;
+    },
+  },
+};
+</script>
